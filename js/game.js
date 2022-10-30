72162b73e7f7d369e0145a52bf84e7698310d296
Yuri <yuri.vynnyk@gmail.com>
2022-10-30 18:11:57 +0200

    change quest history. add state for statistic

--- js/game.js
+++ js/game.js
@@ -62,25 +62,25 @@ const textNodes = [
     text: 'Ви берете участь у секретній місії під назвою "Кінець війні". Місія дуже небезпечна. Перед вами стоїть остання можливість відмовитись. Після підтверждення вас відправлять у Британію.',
     options: [
       {
-        text: 'Ні, я не вірю в себе',
+        text: 'Ні, я не вірю в себе.',
         nextText: 1.2
       },
       {
-        text: 'Так, я хочу якнайшвидшої перемоги України',
+        text: 'Так, я хочу якнайшвидшої перемоги України!',
         nextText: 2
       },
       {
-        text: 'Я зможу виконати місію без поїздки в Британію',
+        text: 'Я зможу виконати місію без поїздки в Британію.',
         nextText: 1.1
       }
     ]
   },
   {
     id: 1.1,
-    text: 'В Британії вас підготують і видадуть необхідні для виконання місії речі',
+    text: 'В Британії вас підготують і видадуть необхідні для виконання місії речі. Ми рекомендуємо спочатку підготуватись до місії.',
     options: [
       {
-        text: 'Ні, я руками задушу Путіна',
+        text: 'Ні, я руками задушу Путіна.',
         nextText: 1.3
       },
       {
@@ -104,7 +104,7 @@ const textNodes = [
     text: 'Ви потрапили до Росії, але у вас немає навичок та предметів, щоб наблизитись до Путіна. Вас вбивають, коли ви намагаєтесь проникнути в Кремль.',
     options: [
       {
-        text: 'Почати спочатку',
+        text: 'Почати спочатку.',
         nextText: -1
       }
     ]
@@ -114,36 +114,36 @@ const textNodes = [
     text: 'Ви прибули в Лондон. Від подальшого вибору залежатиме успішність місії.',
     options: [
       {
-        text: 'Відправитися в школу чародійства Hogwarts (так так, вона існує) і освоїти навички чаклування',
+        text: 'Відправитись в школу чародійства Hogwarts (так так, вона існує) і освоїти навички чаклування!',
         nextText: 3
       },
       {
-        text: 'Відправитися в МІ-6 і освоїти навички розвідки',
+        text: 'Відправитися в МІ-6 і освоїти навички розвідки!',
         nextText: 4
       },
       {
-        text: 'Відправитися в казарми, де Ви оволодієте навичками тактики бою і володіння зброєю',
+        text: 'Відправитися в казарми, де Ви оволодієте навичками тактики бою і володіння зброєю!',
         nextText: 5
       }
     ]
   },
   {
     id: 3,
-    text: 'Ви прибули до школи чародійства. Всі дуже раді вас бачити і кожен хоче допомогти. Дуже хочется вивчити все, але немає на це часу і треба вибирати. Вже скоро треба переправлятись в Росію через таємний тунель з Фінляндії',
+    text: 'Ви прибули до школи чародійства. Всі дуже раді вас бачити і кожен хоче допомогти. Дуже хочется вивчити все, але немає на це часу і треба вибирати. Вже скоро треба переправлятись в Росію через таємний тунель з Фінляндії.',
     options: [
       {
-        text: 'Пройти курс по зілляварення та отримати чарівну паличку, трави і колбу',
-        setState: { herbsАndPotionFlask: true, magicWand: true, potions: true },
+        text: 'Пройти курс по зілляварення та отримати чарівну паличку, трави і колбу.',
+        setState: { items: { herbsАndPotionFlask: 'трави та флакон із зіллям', magicWand: 'магічна паличка', potions: 'зілляварення' } },
         nextText: 6
       },
       {
-        text: 'Прийняти подарунок мантію невидимку, чарівну паличку і мапу мародерів',
-        setState: { invisibilityCloak: true, magicWand: true, maraudersMap: true },
+        text: 'Прийняти подарунок мантію невидимку, чарівну паличку і мапу мародерів.',
+        setState: { items: { invisibilityCloak: 'плащ невидимка', magicWand: 'магічна паличка', maraudersMap: 'мапа мародерів' } },
         nextText: 6
       },
       {
-        text: 'Отримати чарівну паличку та вивчити закляття Авада Кедавра',
-        setState: { magicWand: true, deathSpells: true },
+        text: 'Отримати чарівну паличку та вивчити закляття Авада Кедавра.',
+        setState: { items: { magicWand: 'магічна паличка', deathSpells: 'заклинання смерті Avada Kedavra' } },
         nextText: 6
       },
     ]
@@ -153,119 +153,120 @@ const textNodes = [
     text: 'Ви володієте навичками розвідки і ніндзюцу і можете відправитись в Росію. Тепер ви зможете з легкісттю проникнути до Путіна.',
     options: [
       {
-        text: 'Взяти з собою сюрікен',
-        setState: { shuriken: true, ninja: true },
+        text: 'Взяти з собою сюрікен.',
+        setState: { items: { shuriken: 'сюрикен', ninja: 'навички ніндзя' } },
         nextText: 6
       },
       {
-        text: 'Взяти з собою ніж',
-        setState: { knife: true, ninja: true },
+        text: 'Взяти з собою ніж.',
+        setState: { items: { knife: 'ніж', ninja: 'навички ніндзя' } },
         nextText: 6
       },
       {
-        text: 'Взяти з собою кунай',
-        setState: { kunai: true, ninja: true },
+        text: 'Взяти з собою кунай.',
+        setState: { items: { kunai: 'кунай', ninja: 'навички ніндзя' } },
         nextText: 6
       },
       {
-        text: 'Взяти з собою пістолет і глушник',
-        setState: { gun: true, ninja: true },
+        text: 'Взяти з собою пістолет і глушник.',
+        setState: { items: { gun: 'пістолет і глушник', ninja: 'навички ніндзя' } },
         nextText: 6
       }
     ]
   },
   {
     id: 5,
-    text: 'Тепер ви ідеальний солдат. Вам пропонують взяти з собою будь-яку зброю і відправитись в Росію через підземний тонель з Фінляндії',
+    text: 'Тепер ви ідеальний солдат. Вам пропонують взяти з собою будь-яку зброю і відправитись в Росію через підземний тонель з Фінляндії.',
     options: [
       {
-        text: 'Взяти автомат',
-        setState: { superGun: true },
+        text: 'Взяти автомат.',
+        setState: { items: { superGun: 'ідеальний солдат', tactics: 'навички тактики у бою', automatic: 'автомат' } },
         nextText: 6
       },
       {
-        text: 'Взяти гранатомет',
-        setState: { superGun: true },
+        text: 'Взяти гранатомет.',
+        setState: { items: { superGun: 'ідеальний солдат', tactics: 'навички тактики у бою', grenadeGun: 'гранатомет' } },
         nextText: 6
       },
       {
-        text: 'Взяти пулемет',
-        setState: { superGun: true, hardWeapon: true },
+        text: 'Взяти пулемет.',
+        setState: { items: { superGun: 'ідеальний солдат', tactics: 'навички тактики у бою', hardWeapon: 'володіння навичками використання важкої зброї' } },
         nextText: 5.1
       }
     ]
   },
   {
     id: 5.1,
-    text: 'Пулемет виявився занадто важким...Варто змінити його на іншу зброю.',
+    text: `Пулемет виявився занадто важким...Варто змінити його на іншу зброю.`,
     options: [
       {
-        text: 'Взяти іншу зброю',
-        nextText: 5
+        text: 'Взяти іншу зброю.',
+        nextText: 5,
       },
       {
-        text: 'Продовжити місію з пулеметом',
+        text: 'Продовжити місію з пулеметом.',
         nextText: 6
       }
     ]
   },
   {
     id: 6,
-    text: 'Ви пробрались в Кремль і знаходитесь в безпечній кімнаті. По даним від розвідки США Путін знаходиться в своєму кабінеті поверхом вище. По дорозі багато охорони. Що будете робити? В цей момент до кімнати хтось підходить. Я чую голос Шойгу.',
+    text: 'Ви пробралися в Кремль і знаходитесь в безпечній кімнаті. По даним від розвідки США Путін знаходиться в своєму кабінеті поверхом вище. По дорозі багато охорони. Що будете робити? В цей момент до кімнати хтось підходить. Я чую голос Шойгу.',
     options: [
       {
         text: 'Відкриваю двері з іншої сторони кімнати і заходжу в неї.',
-        requiredState: (currentState) => currentState.herbsАndPotionFlask,
+        requiredState: (currentState) => currentState.items.herbsАndPotionFlask,
         nextText: 6.1
       },
       {
-        text: "Коли він зайшов до кімнати я підкрадаюсь позаду і вирубаю його. Беру волос Шойгу, варю зілля і випиваю його. Теперь я зовні один в один як Шойгу. Їдемо на аудієнцію до Путіна.",
-        requiredState: (currentState) => currentState.herbsАndPotionFlask,
+        text: "Коли він зайшов до кімнати я підкрадаюсь позаду і вирубаю його. Беру волос Шойгу, варю зілля і випиваю його. Теперь я зовні один в один як Шойгу. Йдемо на аудієнцію до Путіна.",
+        requiredState: (currentState) => currentState.items.herbsАndPotionFlask,
         nextText: 8
       },
       {
-        text: 'Надягаю мантію невидимку і виходжу в коридор розминувшись з Шойгу. Хоча він такий дурень, що без мантії не побачив би мене))',
-        requiredState: (currentState) => currentState.invisibilityCloak,
+        text: 'Надягаю мантію невидимку і виходжу в коридор розминувшись з Шойгу. Хоча він такий дурень, що і без мантії не побачив би мене :)',
+        requiredState: (currentState) => currentState.items.invisibilityCloak,
         nextText: 7
       },
       {
         text: 'Він заходить і я використовую закляття Авада Кедавра. Шойгу падає... Я виходжу в коридор.',
-        requiredState: (currentState) => currentState.deathSpells,
+        requiredState: (currentState) => currentState.items.deathSpells,
         nextText: 7
       },
       {
         text: 'Використовуючи навички ніндзюцу підкрадась до Шойгу і знешкоджую його. Виходжу в коридор.',
-        requiredState: (currentState) => currentState.ninja,
+        requiredState: (currentState) => currentState.items.ninja,
         nextText: 7
       },
       {
         text: 'Використовую зброю, що взяв з собою з Британії і вбиваю Шойгу.',
-        requiredState: (currentState) => currentState.ninja,
+        requiredState: (currentState) => currentState.items.ninja,
         nextText: 7
       },
       {
         text: 'Відкриваю двері з іншої сторони кімнати і заходжу в неї.',
-        requiredState: (currentState) => currentState.hardWeapon,
+        requiredState: (currentState) => currentState.items.hardWeapon,
         nextText: 6.1
       },
       {
         text: 'Як заходить Шойга стріляю в нього і вибігаю в коридор',
-        requiredState: (currentState) => !currentState.hardWeapon && currentState.superGun,
+        requiredState: (currentState) => !currentState.items.hardWeapon && currentState.items.superGun,
         nextText: 7
       }
     ]
   },
   {
     id: 6.1,
-    text: 'Ви потрапляєте в невеличку кімнату, з якої немає виходу. Це глухий кут. Треба повернутися назад',
+    text: 'Ви потрапляєте в невеличку кімнату, з якої немає виходу. Це глухий кут. Треба повернутися назад.',
     options: [
       {
-        text: 'Повернутися назад',
+        text: 'Повернутися назад.',
         nextText: 6
       },
       {
-        text: 'Ви занадто стомлені і засипаєте',
-        requiredState: (currentState) => currentState.hardWeapon,
+        text: 'Ви занадто стомлені і засипаєте...',
+        requiredState: (currentState) => currentState.items.hardWeapon,
+        setState: { death: 'Ви не виконали місію і потрапили у полон.' },
         nextText: 6.2
       },
     ]
@@ -286,42 +287,46 @@ const textNodes = [
     options: [
       {
         text: 'Намагаюся їх всіх вбити за допомогою закляття Авада Кедавра.',
-        requiredState: (currentState) => currentState.deathSpells,
+        requiredState: (currentState) => currentState.items.deathSpells,
+        setState: { death: 'Ви померли і не виконали місію.' },
         nextText: 7.1
       },
       {
         text: 'Спокійно проходжу повз них. Оце так річь ця мантія! Дивлячись на мапу мародерів йду до Путіна робити йому сюрприз.',
-        requiredState: (currentState) => currentState.invisibilityCloak,
+        requiredState: (currentState) => currentState.items.invisibilityCloak,
         nextText: 8
       },
       {
-        text: 'Спокійно проходжу повз них. Оце так річь ця мантія! Дивлячись на мапу мародерів хочу спочатку встановити український прапор на Кремлі (оце так новини будуть зранку), а потім на зустріч з Путіним',
-        requiredState: (currentState) => currentState.invisibilityCloak,
+        text: 'Спокійно проходжу повз них. Оце так річь ця мантія! Дивлячись на мапу мародерів хочу спочатку встановити український прапор на Кремлі (оце так новини будуть зранку), а потім на зустріч з Путіним.',
+        requiredState: (currentState) => currentState.items.invisibilityCloak,
         nextText: 8
       },
       {
-        text: 'Проводжу диверсійну роботу, відвертаю увагу охоронців і проходжу по коридору',
-        requiredState: (currentState) => currentState.ninja,
+        text: 'Проводжу диверсійну роботу, відвертаю увагу охоронців і проходжу по коридору.',
+        requiredState: (currentState) => currentState.items.ninja,
         nextText: 8
       },
       {
-        text: 'Використовуючи всі свої навички, якими навчили в Британії вбиваю всіх охоронців і проходжу по коридору',
-        requiredState: (currentState) => currentState.ninja,
+        text: 'Використовуючи всі свої навички, якими навчили в Британії вбиваю всіх охоронців і проходжу по коридору.',
+        requiredState: (currentState) => currentState.items.ninja,
         nextText: 8
       },
       {
-        text: 'Хочу спочатку встановити український прапор на Кремлі (оце так новини будуть зранку), а потім на зустріч з Путіним',
-        requiredState: (currentState) => currentState.ninja,
+        text: 'Хочу спочатку встановити український прапор на Кремлі (оце так новини будуть зранку), а потім на зустріч з Путіним.',
+        requiredState: (currentState) => currentState.items.ninja,
+        setState: { death: 'Ви померли і не виконали місію, але завдяки вашим діям розпочалася революція всередині країни.' },
         nextText: 7.2
       },
       {
-        text: 'Влаштовую перестрілку з охоронцями',
-        requiredState: (currentState) => !currentState.hardWeapon && currentState.superGun,
+        text: 'Влаштовую перестрілку з охоронцями.',
+        requiredState: (currentState) => !currentState.items.hardWeapon && currentState.items.superGun,
+        setState: { death: 'Ви померли і не виконали місію.' },
         nextText: 7.1
       },
       {
-        text: 'Вирішую, що спочатку встановити український прапор на Кремлі',
-        requiredState: (currentState) => !currentState.hardWeapon && currentState.superGun,
+        text: 'Вирішую, що спочатку встановити український прапор на Кремлі.',
+        requiredState: (currentState) => !currentState.items.hardWeapon && currentState.items.superGun,
+        setState: { death: 'Ви померли і не виконали місію, але завдяки вашим діям розпочалася революція всередині країни.' },
         nextText: 7.2
       }
     ]
@@ -331,7 +336,7 @@ const textNodes = [
     text: 'Нажаль, охоронців занадто багато і ми падаємо від куль одного з охоронців. Остання наша думка: треба було брати мантію невидимку...',
     options: [
       {
-        text: 'Почати спочатку',
+        text: 'Почати спочатку.',
         nextText: -1
       }
     ]
@@ -341,7 +346,7 @@ const textNodes = [
     text: 'Встановлюємо прапор на Кремлі, це бачать росіяни. Нас вбивають охоронці, але запізно. Починається революція на Червоній площі.',
     options: [
       {
-        text: 'Хоча Путіна Ви не вбили, але почали революцію всередині країни і стали героєм. Почати спочатку',
+        text: 'Хоча Путіна Ви не вбили, але почали революцію всередині країни і стали героєм. Почати спочатку.',
         nextText: -1
       }
     ]
@@ -352,32 +357,38 @@ const textNodes = [
     options: [
       {
         text: 'Знаходжу важкий предмет на столі і вбиваю Путіна',
-        requiredState: (currentState) => currentState.invisibilityCloak,
+        requiredState: (currentState) => currentState.items.invisibilityCloak,
+        setState: { death: 'Ви вбили Путіна.' },
         nextText: 9
       },
       {
-        text: 'Під плащем жбурляю в Путіна всі предмети, які потрапляють під руку. Путін божеволіє і вмирає від сердцевого нападу. В новинах розкажуть, що помер на роботі)',
-        requiredState: (currentState) => currentState.invisibilityCloak,
+        text: 'Під плащем жбурляю в Путіна всі предмети, які потрапляють під руку. Путін божеволіє і вмирає від сердцевого нападу. В новинах розкажуть, що помер на роботі :)',
+        requiredState: (currentState) => currentState.items.invisibilityCloak,
+        setState: { death: 'Ви інкременували смерть Путіна.' },
         nextText: 9
       },
       {
-        text: 'Вбиваю Путіна зброєю отриманою в Британії',
-        requiredState: (currentState) => currentState.ninja,
+        text: 'Вбиваю Путіна зброєю отриманою в Британії.',
+        requiredState: (currentState) => currentState.items.ninja,
+        setState: { death: 'Ви вбили Путіна.' },
         nextText: 9
       },
       {
         text: 'Швидкими рухамі за кілька секунд підбігаю до Путіна, вирубаю його і встановлюю сцену самогубства.',
-        requiredState: (currentState) => currentState.ninja,
+        requiredState: (currentState) => currentState.items.ninja,
+        setState: { death: 'Ви інкременували смерть Путіна.' },
         nextText: 9
       },
       {
         text: "Вбиваю Путіна і виходячи з кабінету спеціально проходжу повз камеру. Ранкові новини розкажуть як Шойгу вбив Путіна.",
-        requiredState: (currentState) => currentState.herbsАndPotionFlask,
+        requiredState: (currentState) => currentState.items.herbsАndPotionFlask,
+        setState: { death: 'Ви інкременували смерть Путіна.' },
         nextText: 9
       },
       {
         text: "Під час розмиви з Путіним непомітно підсипаю йому отруту, яку він випиває. Путін падає на підлогу, я гукаю на допомогу і під час суматохи спокійно виходжу з кімнати.",
-        requiredState: (currentState) => currentState.herbsАndPotionFlask,
+        requiredState: (currentState) => currentState.items.herbsАndPotionFlask,
+        setState: { death: 'Ви інкременували смерть Путіна.' },
         nextText: 9
       },
     ]
